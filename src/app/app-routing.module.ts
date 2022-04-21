3b3ba8e145dfee4691f4dda632634bc425f8de66
Błażej Skrzypniak <blazej.skrzypniak@com40.pl>
2022-04-21 14:37:54 +0200

    Passport

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: 'posts',
     loadChildren: () => import('./post/post.module').then(m => m.PostModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
   }
 ];
 
