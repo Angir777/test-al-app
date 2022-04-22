6143df81b5fa2504240e45500c1310a6b799435a
Błażej Skrzypniak <blazej.skrzypniak@com40.pl>
2022-04-22 14:20:23 +0200

    Autoryzacja na wybranej trasie

--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from '../shared/services/auth/auth-guard.service';
+import { GuestGuardService } from '../shared/services/auth/guest-guard.service';
 import { PostsComponent } from './posts/posts.component';
 import { ShowPostComponent } from './show-post/show-post.component';
 import { UpdatePostComponent } from './update-post/update-post.component';
@@ -7,7 +9,8 @@ import { UpdatePostComponent } from './update-post/update-post.component';
 const routes: Routes = [
   {
     path: '',
-    component: PostsComponent
+    component: PostsComponent,
+    canActivate: [ AuthGuardService ]
   },
   {
     path: 'new',
